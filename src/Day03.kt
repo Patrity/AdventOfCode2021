fd2a886d51c3e9513948f2e0f481cf7ee0c4eaca
Tony Costanzo <tonycos92@gmail.com>
2021-12-03 22:19:33 -0600

    Day 3 - Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -8,14 +8,49 @@ fun main() {
         return if (count < 0) 0 else 1
     }
 
+    fun remove(input: MutableList<String>, index: Int, type: String) {
+        val common = findCommon(input, index)
+
+        when(type) {
+            "oxygen" -> {
+                input.removeIf {
+                    it[index] == if (common == 1) '1' else '0'
+                }
+            }
+            "co2" -> {
+                input.removeIf {
+                    it[index] == if (common == 1) '0' else '1'
+                }
+            }
+        }
+    }
+
 
     val input = readInput("Day03")
     val testInput = readInput("Day03_test")
+
+    val oxygenList = input.toMutableList()
+    val co2List = input.toMutableList()
 
     val size = input[0].length
 
     val epsilon = StringBuilder()
     val gamma = StringBuilder()
+
+    for (pos in 0 until size) {
+        if (oxygenList.size == 1 && co2List.size == 1) {
+            break
+        }
+        if (oxygenList.size != 1) {
+            remove(oxygenList, pos, "oxygen")
+        }
+        if (co2List.size != 1) {
+            remove(co2List, pos, "co2")
+        }
+
+    }
+    val oxygenValue = oxygenList.first().toInt(2)
+    val co2Value = co2List.first().toInt(2)
 
     for (pos in 0 until size) {
         val char = findCommon(input, pos)
@@ -25,6 +60,7 @@ fun main() {
     val epsilonValue = Integer.parseInt(epsilon.toString(), 2)
     val gammaValue = Integer.parseInt(gamma.toString(), 2)
     println(epsilonValue * gammaValue)
+    println(oxygenValue * co2Value)
 
 
 }
